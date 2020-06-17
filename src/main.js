d4d65665f9e63ec9b94405e7f9a54012275fde13
Kirsten Richard <kr694229@dal.ca>
2020-06-17 10:06:43 -0300

    Addressed quotations and config file

--- src/main.js
+++ src/main.js
@@ -15,33 +15,33 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import SocialSharing from 'vue-social-sharing';
-import VueGitHubButtons from 'vue-github-buttons';
-import 'vue-github-buttons/dist/vue-github-buttons.css';
-import App from './App.vue';
-import '@/assets/scss/white-dashboard.scss';
-import '@/assets/css/nucleo-icons.css';
-import '@/assets/demo/demo.css';
-import '@/components/Filters/Filter';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import SocialSharing from "vue-social-sharing";
+import VueGitHubButtons from "vue-github-buttons";
+import "vue-github-buttons/dist/vue-github-buttons.css";
+import App from "./App.vue";
+import "@/assets/scss/white-dashboard.scss";
+import "@/assets/css/nucleo-icons.css";
+import "@/assets/demo/demo.css";
+import "@/components/Filters/Filter";
 
-import GlobalComponents from './globalComponents';
-import GlobalDirectives from './globalDirectives';
-import RTLPlugin from './RTLPlugin';
-import Notify from '@/components/NotificationPlugin';
-import i18n from './i18n';
-import SideBar from '@/components/SidebarPlugin';
+import GlobalComponents from "./globalComponents";
+import GlobalDirectives from "./globalDirectives";
+import RTLPlugin from "./RTLPlugin";
+import Notify from "@/components/NotificationPlugin";
+import i18n from "./i18n";
+import SideBar from "@/components/SidebarPlugin";
 
 Vue.config.productionTip = false;
 
 // router setup
-import routes from './router';
+import routes from "./router";
 
 // configure router
 const router = new VueRouter({
-	routes, // short for routes: routes
-	linkExactActiveClass: 'active',
+  routes, // short for routes: routes
+  linkExactActiveClass: "active",
 });
 
 Vue.use(VueRouter);
@@ -54,7 +54,7 @@ Vue.use(SideBar);
 Vue.use(Notify);
 
 new Vue({
-	router,
-	i18n,
-	render: h => h(App),
-}).$mount('#app');
+  router,	
+  i18n,
+  render: (h) => h(App),
+}).$mount("#app");
